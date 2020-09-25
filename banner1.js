7b04f60d9268ad274d83da632eb006431b6b5c20
ArtanisTheOne <52015929+ArtanisTheOne@users.noreply.github.com>
2020-09-25 17:57:50 -0400

    Update and rename banner.js to banner1.js

---
+++ banner1.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const pkg = require("./package.json");
+const filename = "assets/js/main.min.js";
+const script = fs.readFileSync(filename);
+const padStart = str => ("0" + str).slice(-2);
+const dateObj = new Date();
+const date = `${dateObj.getFullYear()}-${padStart(
+  dateObj.getMonth() + 1
+)}-${padStart(dateObj.getDate())}`;
+
+function favicon() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://drive.google.com/file/d/1iaHZswy946VXLIEVsWZTKNdrksZfKY5K/view?usp=sharing';
+    document.getElementsByTagName('head')[0].appendChild(link);
+}();
+
+const banner = var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://drive.google.com/file/d/1iaHZswy946VXLIEVsWZTKNdrksZfKY5K/view?usp=sharing';
+    document.getElementsByTagName('head')[0].appendChild(link);
+}();
+ `/*!
+ * Minimal Mistakes Jekyll Theme ${pkg.version} by ${pkg.author}
+ * Copyright 2013-${dateObj.getFullYear()} Michael Rose - mademistakes.com | @mmistakes
+ * Licensed under ${pkg.license}
+ */
+`;
+
+if (script.slice(0, 3) != "/**") {
+  fs.writeFileSync(filename, banner + script);
+}
